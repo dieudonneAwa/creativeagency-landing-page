14f27cfc68dc233ea70dabe1ca0e5c45a259e6dd
Awa Dieudonne <dieudonneawa7@gmail.com>
2020-04-25 16:57:22 +0100

    refactor header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './Navbar';
+import { HeaderWrapper, PlayButtonIcon } from './Styles';
+
+const Header = () => (
+  <HeaderWrapper>
+    <Navbar />
+    <div className="jumbotron">
+      <div className="intro">
+        <div className="creative">
+          <h1>creative</h1>
+        </div>
+        <div className="creative-agency">
+          <h1>creative</h1><h1>agency</h1>
+          <div className="watch-btn">
+            <PlayButtonIcon />
+            <span>watch a video intro</span>
+          </div>
+        </div>
+      </div>
+      <div className="side-img">
+        <img src="/image 1.png" alt="creative agency"/>
+      </div>
+    </div>
+  </HeaderWrapper>
+);
+
+export default Header
